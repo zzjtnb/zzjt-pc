fff44e4b0263e9d9a36329acda329bdcda83fce6
Brice <zhengzhujituan@gmail.com>
2019-05-24 23:51:08 +0800

    设置自动换行

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,39 @@
 <template>
-    <div id="app">
-        <router-view/>
-    </div>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
 <script>
-    import { mapGetters } from 'vuex'
-    export default {
-        name: 'App',
-        computed: {
-            ...mapGetters([
-                'githubUsername',
-                'htmlTitle'
-            ])
-        },
-        created() {
-            this.$store.dispatch("Init")
-            this.$store.dispatch("GetInfo")
-            this.$setTitle(this.$route.meta.title)
-            let windowSize = this.$util.getWindowSize()
-            let pathArr = this.$route.path.split("/")
-            if (pathArr[1] == "user" && windowSize.height > windowSize.width * 1.2) {
-                this.$router.push("/mobile/user/blog")
-            }
-            if (pathArr[1] == "mobile" && windowSize.height <= windowSize.width * 1.2) {
-                this.$router.push("/")
-            }
-        }
+import { mapGetters } from 'vuex'
+export default {
+  name: 'App',
+  computed: {
+    ...mapGetters([
+      'githubUsername',
+      'htmlTitle'
+    ])
+  },
+  created () {
+    this.$store.dispatch("Init")
+    this.$store.dispatch("GetInfo")
+    this.$setTitle(this.$route.meta.title)
+    let windowSize = this.$util.getWindowSize()
+    let pathArr = this.$route.path.split("/")
+    if (pathArr[1] == "user" && windowSize.height > windowSize.width * 1.2) {
+      this.$router.push("/mobile/user/blog")
     }
+    if (pathArr[1] == "mobile" && windowSize.height <= windowSize.width * 1.2) {
+      this.$router.push("/")
+    }
+  }
+}
 </script>
+<style>
+.markdown-body pre {
+  white-space: pre-wrap;
+}
+.markdown-body pre code {
+  white-space: pre-wrap;
+}
+</style>
