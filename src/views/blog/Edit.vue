9730e49e4166d3cb94b7bf5b85d3b1f16e2a687d
争逐 <zhengzhujituan@gmail.com>
2019-11-11 22:06:04 +0800

    修改标题长度

--- src/views/blog/Edit.vue
+++ src/views/blog/Edit.vue
@@ -1,25 +1,35 @@
 <template>
-  <div style="min-height: 600px" v-loading="loading">
-    <el-card shadow="never" style="margin-bottom: 20px">
-      <el-form ref="form" :model="form" label-width="80px" :rules="ruleValidate">
-        <el-form-item label="标题" prop="title">
-          <!-- <el-input v-model="form.title" :disabled="true"></el-input> -->
-          <el-input v-model="form.title" ></el-input>
-        </el-form-item>
-        <el-form-item label="简要说明" prop="description">
-          <el-input v-model="form.description" type="textarea"></el-input>
-        </el-form-item>
-        <el-form-item label="博客正文" prop="content">
-          <mavon-editor @imgAdd="imgAdd" style="max-height: 500px" ref="md" v-model="form.content" :subfield="false" :toolbars="mavonEditorToolbars" :ishljs="true" :codeStyle="true" codeStyle="agate" />
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="onSubmit" :loading="submitButton.loading" :disabled="submitButton.disabled">修改</el-button>
-          <el-button @click="$router.push('/user/blog/main')">返回</el-button>
-        </el-form-item>
-      </el-form>
-    </el-card>
-    <token-dialog ref="tokenDialog"></token-dialog>
-  </div>
+	<div style="min-height: 600px" v-loading="loading">
+		<el-card shadow="never" style="margin-bottom: 20px">
+			<el-form ref="form" :model="form" label-width="80px" :rules="ruleValidate">
+				<el-form-item label="标题" prop="title">
+					<!-- <el-input v-model="form.title" :disabled="true"></el-input> -->
+					<el-input v-model="form.title"></el-input>
+				</el-form-item>
+				<el-form-item label="简要说明" prop="description">
+					<el-input v-model="form.description" type="textarea"></el-input>
+				</el-form-item>
+				<el-form-item label="博客正文" prop="content">
+					<mavon-editor
+						@imgAdd="imgAdd"
+						style="max-height: 500px"
+						ref="md"
+						v-model="form.content"
+						:subfield="false"
+						:toolbars="mavonEditorToolbars"
+						:ishljs="true"
+						:codeStyle="true"
+						codeStyle="agate"
+					/>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="primary" @click="onSubmit" :loading="submitButton.loading" :disabled="submitButton.disabled">修改</el-button>
+					<el-button @click="$router.push('/user/blog/main')">返回</el-button>
+				</el-form-item>
+			</el-form>
+		</el-card>
+		<token-dialog ref="tokenDialog"></token-dialog>
+	</div>
 </template>
 <script>
 import { mapGetters } from 'vuex'
@@ -40,7 +50,7 @@ export default {
       ruleValidate: {
         title: [
           { required: true, message: '请输入标题', trigger: 'blur' },
-          { type: 'string', max: 32, message: '标题长度不大于32字符', trigger: 'change' }
+          { type: 'string', max: 100, message: '标题长度不大于100字符', trigger: 'change' }
         ],
         description: [
           { required: true, message: '请输入博客描述', trigger: 'blur' }
